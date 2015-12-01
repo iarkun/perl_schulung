8125359a127835d146bd2aef8e91f4f30bf05788
Ilyas Arkun <iarkun@gmail.com>
2015-12-01 17:49:44 +0100

    Save

--- dienstag/servicetexte.pl
+++ dienstag/servicetexte.pl
@@ -9,11 +9,26 @@ sub print_service
  open my $fh, '<', $path or die "Kann Datei nicht oeffnen!\n"; 
  while (my $line = <$fh>){
   chomp $line;
-  next unless $line; #Leerzeilen ignorieren
+#  next unless $line; #Leerzeilen ignorieren
 #  next if substr($line,0,1) eq "#"; #Kommentarzeilen ignorieren
 #  next unless index($line, "#"); #Kommentarzeilen ignorieren
-  next if $line =~ m/^#/; # ^=Zeilenanfang, #=#
-  printf "%s\n", $line;
+#  next if $line =~ m/^#/; # ^=Zeilenanfang, #=#
+  next unless $line =~ m/^ #Zeilenanfang
+		[^\#]  	#Kein # am Zeilenanfang, sonst jedes Zeichen
+		[^\#]* 	#Dann beliebig viele Nicht -#-Zeichen
+			#Man kann auch anstelle der beiden Zeilen ueber dieser
+			# [^\#]+ schreiben, also Mindestens ein Nicht-#-Zeichen
+			#Im Prinzip aber beliebig viele
+		\#	#Dann genau ein #-Zeichen
+		\s*	#Gefolgt von beliebig vielen Leerzeichen, keins ist auch OK
+		(.*)	#Beliebig viele beliebige Zeichen
+			#Wegen Gruppierung () in $1 gespeichert
+		$	#...Bis zum Ende der Zeile
+		/x;	#Extended RegEX
+#.*?#s*(.*)$
+#  ^[^#][^#]*#\s*(.*)$
+  printf "%s\n", $1;
+#  printf "%s\n", $line;
  }
  close($fh);
 }
